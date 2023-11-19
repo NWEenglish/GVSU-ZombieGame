0125cc7b82cf72d6d2e1f58ce5c6f1b02d296329
Nicholas <englishnicholas@yahoo.com>
2023-11-19 03:06:22 -0500

    Implement the actual logging logic [gen-622]

--- ZombieGame/Assets/Scripts/Singletons/Logger.cs
+++ ZombieGame/Assets/Scripts/Singletons/Logger.cs
@@ -2,6 +2,7 @@
 
 namespace Assets.Scripts.Singletons
 {
+    // Per Unity, this will output to both the Unity terminal and in the "AppData\Roaming" directory.
     internal class Logger
     {
         private static Logger instance = null;
@@ -20,12 +21,12 @@ namespace Assets.Scripts.Singletons
 
         public void LogDebug(string message)
         {
-            throw new NotImplementedException();
+            UnityEngine.Debug.Log(message);
         }
 
         public void LogError(Exception exception, string message)
         {
-            throw new NotImplementedException();
+            UnityEngine.Debug.LogError($"<color=red>Error: </color>{message} | ExceptionMessage: {exception?.Message}");
         }
     }
 }
