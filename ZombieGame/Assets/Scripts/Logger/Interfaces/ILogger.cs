bba0234ecf83d09783a9a627e5a56ebf4c7f2372
Nicholas <englishnicholas@yahoo.com>
2023-11-19 00:55:55 -0500

    Created interfaces and class for logger [cis-622]

---
+++ ZombieGame/Assets/Scripts/Logger/Interfaces/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Logger.Interfaces
+{
+    public interface ILogger
+    {
+        public void Log(string message);
+    }
+}
---
+++ ZombieGame/Assets/Scripts/Logger/Logger.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Scripts.Logger.Interfaces;
+
+namespace Assets.Scripts.Logger
+{
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+
+        }
+    }
+}
